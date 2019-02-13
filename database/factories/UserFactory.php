98e0a101f1b46b0402cb1bf785d9d9e5d7ce384a
mannv <mannv.g4@gmail.com>
2019-02-14 04:14:55 +0700

    ok

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$EPCguXrR1X8tYqsfud99sO0oIKS.79npa9.a6aVCgoQrXTl8au.la', // abc123
         'remember_token' => str_random(10),
     ];
 });
